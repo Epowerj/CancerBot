3a178e38f0f4502916580cb2069998009e1d454e
Epowerj <epowerj@gmail.com>
2016-06-01 03:01:26 +0000

    fixed typo

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def main():
     dp.add_handler(CommandHandler("time", time))
     dp.add_handler(CommandHandler("boat", boat))
 
-    dp.add_error_handler(error)B
+    dp.add_error_handler(error)
 
     updater.start_polling(timeout=5)
 
