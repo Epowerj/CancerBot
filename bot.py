e10ffe87d5eea6aceab657ca4c9e36199a5f149f
Epowerj <epowerj@gmail.com>
2016-08-31 17:41:24 +0000

    Added boat trigger

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
 from key import apikey
 import datetime, json, random, time
@@ -11,8 +12,6 @@ memers = {}
 savepath = "memers.dict"
 
 ccentral_id = -1001044604031
-
-hoptidote_cooldown = time.monotonic()
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -102,19 +101,12 @@ def memegrab(bot):
 
 def hoptidote(bot, update): #TODO add timeout
     import time
-    global hoptidote_cooldown
 
-    symptoms = {'normie', 'suffering', 'misery', 'comfy'}
-    antidotes = {27511, 27512, 27513, 27514, 27515, 27516, 27517, 27518, 27519, 27520, 27521, 27522, 27523, 27524, 27525, 
+    antidotes = [27511, 27512, 27513, 27514, 27515, 27516, 27517, 27518, 27519, 27520, 27521, 27522, 27523, 27524, 27525, 
                  27526, 27527, 27528, 27529, 27530, 27531, 27532, 27533, 27534, 27535, 27536, 27537, 27538, 27539, 27540,
-                 27541, 27542, 27543, 27544, 27545, 27546, 27547, 27548}
+                 27541, 27542, 27543, 27544, 27545, 27546, 27547, 27548, 27654]
 
-    if update.message.from_user.id == 94250469 and (time.monotonic()-hoptidote_cooldown) > 600:
-        hoptidote_cooldown = time.monotonic()
-        for symptom in symptoms:
-            if symptom in update.message.text:
-                bot.forward_message(chat_id=update.message.chat_id, message_id=antidotes[random.randint(0, antidotes.length)])
-                break
+    bot.forward_message(chat_id=update.message.chat_id, from_chat_id=83218061, message_id=antidotes[random.randint(0,  len(antidotes))])
 
 
 def drop(bot, update):
@@ -126,6 +118,15 @@ def drop(bot, update):
         bot.sendMessage(ccentral_id, text=update.message.from_user.first_name+" has dropped a meme! Use /ebin to catch it!")
     else:
         bot.sendMessage(update.message.chat_id, text="You're out of ebins")
+
+
+def shop(bot, update):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'), InlineKeyboardButton("Option 2", callback_data='2')],
+                                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    bot.sendMessage(update.message.chat_id, text="Please choose:", reply_markup=reply_markup)
 
 
 def gift(bot, update): #TODO
@@ -144,12 +145,13 @@ def top(bot, update):
     for memer in sorted_memers:
         toplist += memer+"\n"
 
-    bot.sendMessage(updage.message.chat_id, text=toplist)
+    bot.sendMessage(update.message.chat_id, text=toplist)
 
 
 def parse(bot, update):
-    #print(update.message.message_id)
-    hoptidote(bot, update)
+    print("Message from " + update.message.from_user.first_name + "(" + str(update.message.from_user.id) + "): " + update.message.text + " (" + str(update.message.message_id) + ")")
+    if " boat" in update.message.text:
+        bot.sendMessage(update.message.chat_id, text="You don't deserve a boat, have this log instead")
 
 
 def error(bot, update, error):
@@ -179,6 +181,7 @@ def main():
     dp.add_handler(CommandHandler("stats", stats))
     dp.add_handler(CommandHandler("chatinfo", chatinfo))
     dp.add_handler(CommandHandler("drop", drop))
+    dp.add_handler(CommandHandler("shop", shop))
 
     dp.add_handler(MessageHandler([Filters.text], parse))
 
