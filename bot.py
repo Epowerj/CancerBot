1a90dfbe530a3b515ebf36ffff2f85ce21787f68
Epowerj <epowerj@gmail.com>
2016-09-20 02:39:57 +0000

    backup

--- bot.py
+++ bot.py
@@ -252,9 +252,6 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_polling(timeout=5)
-    
-    # Wild meme generation
-    #jqueue.put(memegrab, 1800, next_t=0)
 
     # Run the bot until the user presses Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
     updater.idle()
--- key.py
+++ key.py
@@ -1,2 +1,2 @@
 
-apikey = "<apikey>"
+apikey = "173297784:AAHH5wZjZzGHPMjjaYz1gMZeuckSBwXnLZc"
